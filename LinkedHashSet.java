ddf49c570a9528c542db1e5a8398f3c354f5f0f3
Avik Saha Roy <97282235+AvikSahaRoy@users.noreply.github.com>
2023-08-14 12:02:40 +0530

    Create LinkedHashSet.java

---
+++ LinkedHashSet.java
@@ -0,0 +1,13 @@
+import java.util.LinkedHashSet;
+
+class Codechef {
+  public static void main(String[] args) {
+    LinkedHashSet<String> playlist = new LinkedHashSet<String>();
+    playlist.add("Moonlight Sonata");
+    playlist.add("The Godfather");
+    playlist.add("Whiplash");
+    playlist.add("Whiplash");
+
+    System.out.println(playlist);
+  }
+}
